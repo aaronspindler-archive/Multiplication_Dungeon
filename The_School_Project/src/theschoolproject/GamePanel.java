9b648c44dc3fbadbb1aea1bae6ff16425ed2fe0a
xNovax <nova1104@live.com>
2014-10-24 10:42:50 -0400

    Added room class and edited the forms

--- The_School_Project/src/theschoolproject/GamePanel.java
+++ The_School_Project/src/theschoolproject/GamePanel.java
@@ -11,30 +11,18 @@ import java.awt.LayoutManager;
 import java.util.Random;
 import javax.swing.JPanel;
 
-/**
- *
- * @author root
- */
 public class GamePanel extends JPanel {
 
     Random r = new Random();
-    
+
     public GamePanel() {
-    } 
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); 
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
-        g.setColor(Color.red);
-        g.drawRect(0, 0, this.getWidth(), this.getHeight());
-        
-        for(int i = 0; i < 17; i++){
-            for(int h = 0; h < 13; h++){
-                g.setColor(new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
-                g.fillRect(i * 50, h * 50, 50, 50);
-            }
-        }
+        super.paintComponent(g);
+        g.setColor(new Color(150, 75, 0));
+        g.fillRect(0, 0, 910, 710);
     }
 
     public GamePanel(LayoutManager layout) {
---
+++ The_School_Project/src/theschoolproject/Room.java
@@ -0,0 +1,5 @@
+package theschoolproject;
+
+public class Room {
+
+}
