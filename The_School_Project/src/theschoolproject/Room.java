58bfa793992bff754f5664cc320eb942c6bfad6a
pspkazy <pspkazy@gmail.com>
2014-10-24 20:14:27 -0400

    Setting up the basics, room, floor tiles etc...

--- The_School_Project/src/theschoolproject/Room.java
+++ The_School_Project/src/theschoolproject/Room.java
@@ -1,5 +1,10 @@
-package theschoolproject;
+package theschoolproject; //THis will be the dungeon room, 16x11 = 176 tiles
 
 public class Room {
 
+    FloorTile [] tileArry = new FloorTile [176];
+    
+    public Room() {
+        
+    }
 }
