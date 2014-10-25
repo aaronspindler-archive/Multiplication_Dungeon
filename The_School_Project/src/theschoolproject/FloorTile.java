7f523e62fa3f187d0504b16bd97844920d73330e
mattuw <mattuw@gmail.com>
2014-10-24 22:02:11 -0400

    Colors

--- The_School_Project/src/theschoolproject/FloorTile.java
+++ The_School_Project/src/theschoolproject/FloorTile.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package theschoolproject;
+
+import java.awt.Color;
 
 /**
  *
  * @author Arthur
  */
 public class FloorTile {
-    
+
+    int TILE_ID = -1;
+
+    public FloorTile(int id) {
+        this.TILE_ID = id;
+    }
+
+    public boolean isSolid() {
+        switch (TILE_ID) {
+            case 1:
+                return true;
+            case 2: 
+                return false;
+            default:
+                return false;
+        }
+    }
+
+    public Color getColor() {
+        Color c = Color.MAGENTA;
+        switch (TILE_ID) {
+            case 1:
+                c = new Color(65,43,3);
+                break;
+            case 2:
+                c = new Color(130,86,6);
+        }
+        return c;
+    }
 }
