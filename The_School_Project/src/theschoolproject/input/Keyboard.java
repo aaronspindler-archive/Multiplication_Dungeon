a5bfb90655b12183efd3b78dc6b4aa42b899a7df
root <root@k4l1>
2014-11-03 09:15:24 -0500

    Keyboard and menu screen takes 2

---
+++ The_School_Project/src/theschoolproject/input/Keyboard.java
@@ -0,0 +1,29 @@
+package theschoolproject.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener{
+    
+    boolean[] keys = new boolean[525];
+    
+    public boolean isKeyDown(String s){
+        if(s.equals("up")){
+            return keys[KeyEvent.VK_UP];
+        }
+        return false;
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+       keys[e.getKeyCode()] = true;
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
+    
+}
