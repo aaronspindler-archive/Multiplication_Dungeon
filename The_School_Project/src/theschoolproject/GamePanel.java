832327c470c1559818d87eabb5c412563e30b8dd
root <root@k4l1>
2014-11-03 09:13:01 -0500

    Keyboard and menu screen

--- The_School_Project/src/theschoolproject/GamePanel.java
+++ The_School_Project/src/theschoolproject/GamePanel.java
@@ -9,13 +9,18 @@ import java.awt.Graphics;
 import java.awt.LayoutManager;
 import java.util.Random;
 import javax.swing.JPanel;
+import theschoolproject.input.Keyboard;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel{
 
     Random r = new Random();
     FloorTile[][] ft = new FloorTile[17][15];
+    boolean mainMenu = true;
+    boolean gameScreen = false;
+    Keyboard keys = new Keyboard();
     
     public GamePanel() {
+        this.addKeyListener(keys);
           for (int w = 0; w < 17; w++) {
             for (int h = 0; h < 15; h++) {
                 if(w == 0 || w == 16 || h == 0 || h == 12){
@@ -25,17 +30,24 @@ public class GamePanel extends JPanel {
                 }
             }
         }
+          this.setFocusable(true);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);     
+        if(mainMenu){
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        }
+        if(gameScreen){
         for (int w = 0; w < 17; w++) {
             for (int h = 0; h < 15; h++) {
               g.setColor(ft[w][h].getColor());
               g.fillRect(w*50, h*50, 50, 50);
             }
         }
+        }
+        
     }
 
     public GamePanel(LayoutManager layout) {
