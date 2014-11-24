30710bd0eb178f99fa31a044493e1eff1bc0cf80
xNovax <nova1104@live.com>
2014-11-24 08:27:59 -0500

    Removed button and textfield

--- The_School_Project/src/theschoolproject/QuestionTimer.java
+++ The_School_Project/src/theschoolproject/QuestionTimer.java
@@ -27,11 +27,11 @@ public class QuestionTimer {
     }
     
     public final String makeEquation() {
-        String eq = "";
+        String eq;
         int multiplicand = gen.nextInt(QUESTION_RANGE) + 1;
         int multiplier = gen.nextInt(QUESTION_RANGE) + 1;
         int product = multiplicand * multiplier;
-        eq = (multiplicand + "x" + multiplier);
+        eq = (multiplicand + "x" + multiplier + " = ");
         return eq;
     }
 
@@ -42,10 +42,5 @@ public class QuestionTimer {
         g.setColor(Color.black);
         g.drawString("Multiplication Question", 370, 121);
         g.drawString(equation, 370, 201);
-        
-        //Don't use ratchet text boxes and buttons --> They can't use keyboard
-        //It also looks god awful
-        //SchoolProjectForm.checkButton.setVisible(true);
-        //SchoolProjectForm.productTextField.setVisible(true);
     }
 }
